fb662edd98cef21d9bc1f4cc7fe6cab61e7d4bc1
Brian Plewinski <125175059+bplewinski@users.noreply.github.com>
2024-01-15 16:33:33 +0800

    Delete nametags directory

--- nametags/registrants2.py
+++
@@ -1,8 +0,0 @@
-class Registrants:
-    def __init__(self, personDict):
-        self.firstname = personDict['firstname']
-        self.lastname = personDict['lastname']
-        self.job = personDict['position']
-        self.company = personDict['company']
-        self.city = personDict['city']
-        self.state = personDict['state']
